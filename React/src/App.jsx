cbd437ea1fb977c25acf6c9ce8b6069e29dd4d1e
MartuFT <148562667+MartuFT@users.noreply.github.com>
2025-10-30 08:09:46 -0300

    Inicio de proyecto

---
+++ React/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import MiComponente from './MiComponente'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="content-wrapper">
+        <div className="veterinaria-container">
+          <MiComponente />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
